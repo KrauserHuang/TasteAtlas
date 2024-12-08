17185c88d60066036f0e5cb8e785a52862751d1d
Krauser Huang <crescentdence@gmail.com>
2024-12-08 11:21:25 +0800

    Initial Commit

---
+++ TasteAtlas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TasteAtlas
+//
+//  Created by Tai Chin Huang on 2024/12/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TasteAtlasTests/TasteAtlasTests.swift
@@ -0,0 +1,17 @@
+//
+//  TasteAtlasTests.swift
+//  TasteAtlasTests
+//
+//  Created by Tai Chin Huang on 2024/12/8.
+//
+
+import Testing
+@testable import TasteAtlas
+
+struct TasteAtlasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
