f0f0f65eaf1ecb0a915295cd8275fc26516a95dc
Andrew Huang <vivatv.it0001@gmail.com>
2024-12-09 18:20:21 +0800

    [Feature] Change Entry point to TabBarController
    1. TabBarController add mapView as it root view
    2. SceneDelegate use TabBarController as window's rootViewController

--- TasteAtlas/Application/SceneDelegate.swift
+++ TasteAtlas/Application/SceneDelegate.swift
@@ -19,8 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        let navigationController = UINavigationController(rootViewController: ViewController())
-        window.rootViewController = navigationController
+        window.rootViewController = TabBarController()
         window.makeKeyAndVisible()
         
         self.window = window
