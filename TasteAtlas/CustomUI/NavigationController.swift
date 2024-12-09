65cc6a66e41cc2dacc29f499a5d9ce4cf2107620
Andrew Huang <vivatv.it0001@gmail.com>
2024-12-09 17:51:58 +0800

    [Feature] Create CustomUI TabBarController/NavigationController
    1. Add UINavigationController extension to setup nav bar
    2. Add NavigationController extension to override preferredStatusBarStyle/childForStatusBarStyle

---
+++ TasteAtlas/CustomUI/NavigationController.swift
@@ -0,0 +1,37 @@
+//
+//  NavigationController.swift
+//  TasteAtlas
+//
+//  Created by IT-MAC-02 on 2024/12/9.
+//
+
+import UIKit
+
+final class NavigationController: UINavigationController, PresentableView {
+    
+    var transitionManager: UIViewControllerTransitioningDelegate?
+    
+    var receivingFrame: CGRect?
+}
+
+// MARK: -
+extension NavigationController {
+    override var preferredStatusBarStyle: UIStatusBarStyle { visibleViewController?.preferredStatusBarStyle ?? super.preferredStatusBarStyle }
+    override var childForStatusBarStyle: UIViewController? { visibleViewController }
+}
+
+// MARK: -
+extension UINavigationController {
+    /// Set the navigation bar appearance
+    /// - Parameter color: color that set the navigation bar
+    func setNavbarApp(color: UIColor) {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = color
+        appearance.shadowColor = .clear
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().isTranslucent = false
+    }
+}
---
+++ TasteAtlas/CustomUI/TabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarController.swift
+//  TasteAtlas
+//
+//  Created by IT-MAC-02 on 2024/12/9.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabbar()
+    }
+    
+    private func setupTabbar() {
+        
+    }
+}
