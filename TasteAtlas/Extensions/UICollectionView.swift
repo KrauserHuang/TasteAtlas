7fd6fea1bb33dd8bd41834466925aafb536ce15a
Krauser Huang <crescentdence@gmail.com>
2024-12-09 08:47:46 +0800

    [Feature] Create UICollectionView/UIView extension

---
+++ TasteAtlas/Extensions/UICollectionView.swift
@@ -0,0 +1,39 @@
+//
+//  UICollectionView.swift
+//  TasteAtlas
+//
+//  Created by Tai Chin Huang on 2024/12/8.
+//
+
+import UIKit
+
+extension UICollectionView {
+    
+    static let header = UICollectionView.elementKindSectionHeader
+    static let footer = UICollectionView.elementKindSectionFooter
+    
+    func register<T: UICollectionViewCell>(cell: T.Type) {
+        register(T.self, forCellWithReuseIdentifier: T.reuseIdentifier)
+    }
+    
+    func register<T: UICollectionViewListCell>(cell: T.Type) {
+        register(T.self, forCellWithReuseIdentifier: T.reuseIdentifier)
+    }
+    
+    func register<T: UICollectionReusableView>(header: T.Type) {
+        register(T.self, forSupplementaryViewOfKind: UICollectionView.header, withReuseIdentifier: T.reuseIdentifier)
+    }
+    
+    func register<T: UICollectionReusableView>(footer: T.Type) {
+        register(T.self, forSupplementaryViewOfKind: UICollectionView.footer, withReuseIdentifier: T.reuseIdentifier)
+    }
+    
+    func dequeueCell<T: UICollectionViewCell>(at indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withReuseIdentifier: String(describing: T.self), for: indexPath) as? T else { fatalError("Unable to dequeue cell") }
+        return cell
+    }
+    
+    func dequeueFooterView<T: UICollectionReusableView>(_ view: T.Type, at indexPath: IndexPath) -> UICollectionReusableView {
+        dequeueReusableSupplementaryView(ofKind: UICollectionView.footer, withReuseIdentifier: view.reuseIdentifier, for: indexPath)
+    }
+}
---
+++ TasteAtlas/Extensions/UIView.swift
@@ -0,0 +1,10 @@
+//
+//  UIView.swift
+//  TasteAtlas
+//
+//  Created by Tai Chin Huang on 2024/12/8.
+//
+
+import UIKit
+
+extension UIView: ReuseIdentifiable {}
