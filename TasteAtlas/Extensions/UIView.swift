1f6a4fb38e26a57a11534a2f1b04b06873d6eae5
Andrew Huang <vivatv.it0001@gmail.com>
2024-12-10 18:20:37 +0800

    [Feature] Update UIView extension
    1. support convenience init
    2. autolayout pinToSuperView
    3. make roundedCorner
    4. change view into UIImage

--- TasteAtlas/Extensions/UIView.swift
+++ TasteAtlas/Extensions/UIView.swift
@@ -7,4 +7,39 @@
 
 import UIKit
 
+extension UIView {
+    /// Create a view using auto layout
+    /// - Parameter useAutoLayout: A bool value that determines if the view should use auto layout
+    convenience init(useAutoLayout: Bool) {
+        self.init()
+        translatesAutoresizingMaskIntoConstraints = !useAutoLayout
+    }
+    
+    func pinToSuperview(with insets: UIEdgeInsets = .zero, edges: UIRectEdge = .all) {
+        guard let superview = superview else { return }
+        translatesAutoresizingMaskIntoConstraints = false
+        if edges.contains(.top) { topAnchor.constraint(equalTo: superview.topAnchor, constant: insets.top).isActive = true }
+        if edges.contains(.bottom) { bottomAnchor.constraint(equalTo: superview.bottomAnchor, constant: -insets.bottom).isActive = true }
+        if edges.contains(.left) { leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: insets.left).isActive = true }
+        if edges.contains(.right) { trailingAnchor.constraint(equalTo: superview.trailingAnchor, constant: -insets.right).isActive = true }
+    }
+    
+    func roundedView(corners: UIRectCorner = .allCorners, radius: CGFloat) {
+        let maskPath1 = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let maskLayer1 = CAShapeLayer()
+        maskLayer1.frame = bounds
+        maskLayer1.path = maskPath1.cgPath
+        layer.mask = maskLayer1
+    }
+    
+    func asImage(frame: CGRect? = nil, scale: CGFloat = UIScreen.main.scale) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(frame?.size ?? bounds.size, false, scale)
+        drawHierarchy(in: bounds, afterScreenUpdates: false)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+}
+
+// MARK: - ReuseIdentifiable
 extension UIView: ReuseIdentifiable {}
