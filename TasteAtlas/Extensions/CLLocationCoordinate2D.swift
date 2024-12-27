1cbd4f1e59177ffb15e0ee969f422a9287ea20e0
Krauser Huang <crescentdence@gmail.com>
2024-12-27 15:06:16 +0800

    [Feature] Create MapKit related extensions
    1. Create CLLocationCoordinate2D extension(some static data for annotation)
    2. Create MKCoordinateRegion extension(conforms Equatable)

---
+++ TasteAtlas/Extensions/CLLocationCoordinate2D.swift
@@ -0,0 +1,20 @@
+//
+//  CLLocationCoordinate2D.swift
+//  TasteAtlas
+//
+//  Created by Tai Chin Huang on 2024/12/26.
+//
+
+import MapKit
+
+extension CLLocationCoordinate2D {
+    static var work: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: 25.05652690360118, longitude: 121.5995070710151)
+    }
+    static var taiepi101: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: 25.033964, longitude: 121.564472)
+    }
+    static var restaurant: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: 25.036333227045827, longitude: 121.56927902736943)
+    }
+}
---
+++ TasteAtlas/Extensions/MKCoordinateRegion.swift
@@ -0,0 +1,18 @@
+//
+//  MKCoordinateRegion.swift
+//  TasteAtlas
+//
+//  Created by Tai Chin Huang on 2024/12/27.
+//
+
+import Foundation
+import MapKit
+
+extension MKCoordinateRegion: Equatable {
+    public static func == (lhs: MKCoordinateRegion, rhs: MKCoordinateRegion) -> Bool {
+        return lhs.center.latitude == rhs.center.latitude &&
+        lhs.center.longitude == rhs.center.longitude &&
+        lhs.span.latitudeDelta == rhs.span.latitudeDelta &&
+        lhs.span.longitudeDelta == rhs.span.longitudeDelta
+    }
+}
